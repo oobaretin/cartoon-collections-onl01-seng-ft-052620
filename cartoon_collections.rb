9334c3eb248cf4e83b47e5faa79a25c0bdfee602
osagie obaretin <osagie.codes@gmail.com>
2020-06-05 18:34:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,13 +11,34 @@ def roll_call_dwarves(array)
 end
 
 roll_call_dwarves(dwarves)
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  nu_array = []
+  i = 0 
+  while i < array.length 
+    nu_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+summon_captain_planet(planeteer_calls)
+
+
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
+
+long_planeteer_calls(call_screams)
+
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
 
 def find_the_cheese# code an argument here
   # the array below is here to help
